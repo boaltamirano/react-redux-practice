c285e5397d0532e39884d05f8ec24f08707d974c
boaltamirano <brayanaltamiranoperez@gmail.com>
2023-04-25 23:43:10 -0500

    feat: Load pokemons after making the request

--- src/store/slices/pokemon/thunks.js
+++ src/store/slices/pokemon/thunks.js
@@ -1,7 +1,13 @@
-import { startLoadingPokemons } from "./pokemonSlice"
+import { pokemonApi } from "../../../api/pokemonApi";
+import { setPokemons, startLoadingPokemons } from "./pokemonSlice"
 
 export const getPokemons = (page = 0 ) => {
     return async(dispatch, getState) => {
         dispatch(startLoadingPokemons());
+
+        const {data} = await pokemonApi.get(`/pokemon?limit=10&offset=${page*10}`);
+        // console.log(resp);
+
+        dispatch(setPokemons({pokemons: data.results, page: page + 1}));
     }
 }
